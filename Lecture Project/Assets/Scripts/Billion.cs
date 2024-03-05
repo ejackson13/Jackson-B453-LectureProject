d86ca3dc6e38070566afffd81cf1ff131b94961e
ejackson13 <evjack1393@gmail.com>
2024-03-05 17:01:57 -0500

    Checkpoint 6

--- Lecture Project/Assets/Scripts/Billion.cs
+++ Lecture Project/Assets/Scripts/Billion.cs
@@ -6,7 +6,6 @@ using static UnityEditor.PlayerSettings;
 
 public class Billion : MonoBehaviour
 {
-
     public string billionColor;
     public Vector3 moveTo;
     public bool reachedStart;
@@ -24,6 +23,11 @@ public class Billion : MonoBehaviour
     public float maxHealth = 100; // the amount of starting health the billion gets
     public float currentHealth; // the amount of health the billion currently has
     public float clickDamage = 25; // the amount of damage the billion takes when being clicked
+
+    public GameObject bulletPrefab; // the prefab of the bullets that we shoot
+    public float shootDistance = 5f; // the max distance to an enemy billion where a billion will fire
+    public float shootInterval = 1.5f; // the interval on which billions will shoot 
+    private float nextFire = 0; // the time in seconds from game start at which the billion can fire its next shot
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +51,7 @@ public class Billion : MonoBehaviour
             MoveToFlag();
         }
 
+        PointToNearestEnemy();
 
         // check for middle mouse click
         if (Input.GetMouseButtonDown(2))
@@ -114,12 +119,14 @@ public class Billion : MonoBehaviour
                 if (closestFlag.gameObject.GetComponent<BillionFlag>().flagColor != billionColor)
                 {
                     closestFlag = null;
+                    rb.velocity = Vector3.zero;
                 }
             }
             // no flags on the field, make closestFlag null again (should already be null, but this is safe)
             else
             {
                 closestFlag = null;
+                rb.velocity = Vector3.zero;
             }
         }
         // if a flag is set, move toward it
@@ -158,6 +165,7 @@ public class Billion : MonoBehaviour
     // moves billion to start position, pushing other billions out of the way
     private void MoveToStart()
     {
+        /*
         float xDiff = moveTo.x - rigTransform.position.x;
         float yDiff = rigTransform.position.y - moveTo.y;
 
@@ -167,6 +175,38 @@ public class Billion : MonoBehaviour
         rigTransform.position = new Vector3(rigTransform.position.x + xChange, rigTransform.position.y - yChange, rigTransform.position.z);
 
         reachedStart = rigTransform.position.x == moveTo.x && rigTransform.position.y == moveTo.y;
+        */
+
+        float xDiff = moveTo.x - rigTransform.position.x;
+        float yDiff = moveTo.y - rigTransform.position.y;
+
+        float xVel;
+        float yVel;
+        if (Mathf.Abs(xDiff) < .25f)
+        {
+            xVel = 0;
+        } else if (xDiff < 0)
+        {
+            xVel = -.1f;
+        } else
+        {
+            xVel = .1f;
+        }
+
+        if (Mathf.Abs(yDiff) < .25f)
+        {
+            yVel = 0;
+        } else if (yDiff < 0)
+        {
+            yVel = -.1f;
+        } else
+        {
+            yVel = .1f;
+        }
+
+        rb.velocity = new Vector2(xVel, yVel);
+        List<GameObject> allFlags = BillionFlag.GetAllFlagsByColor(billionColor); // check if there are any flags out and don't bother moving to starting point if so
+        reachedStart = (yVel == 0 && xVel == 0) || (allFlags.Count > 0);
     }
 
 
@@ -195,5 +235,83 @@ public class Billion : MonoBehaviour
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+
+
+    private void PointToNearestEnemy()
+    {
+        // Get nearest enemy
+        GameObject nearestEnemy = GetNearestEnemyBillion();
+        if (nearestEnemy == null) // return if there are no enemy billions
+        {
+            return;
+        }
+        Vector2 nearestEnemyPos = nearestEnemy.transform.position;
+
+        Vector2 thisPos = transform.position; // convert the position of this billion to a vector 2
+
+        // get rotation angle
+        Vector2 diff = nearestEnemyPos - thisPos; // get difference between the two vectors
+        float angleRad = Mathf.Atan2(diff.y, diff.x);
+        float angleDeg = angleRad * Mathf.Rad2Deg;
+
+        // update rotation angle
+        transform.eulerAngles = new Vector3(0, 0, angleDeg);
+
+        // shoot if enough time has passed and there is an enemy within range
+        if (Vector2.Distance(thisPos, nearestEnemyPos) <= shootDistance && Time.time >= nextFire)
+        {
+            // spawn at end of turret
+            float distFromCenter = GetComponent<CircleCollider2D>().radius; // get distance from center that bullet should spawn at
+            Vector3 startPos = (Vector2)transform.position + (diff.normalized * distFromCenter); // set start position to be distFromCenter away from the center of the billion
+            startPos.z = transform.position.z;
+
+            // instantiate bullet
+            GameObject bullet = Instantiate(bulletPrefab, startPos, Quaternion.identity);
+
+            // rotate bullet according to direction billion is facing
+            bullet.transform.eulerAngles = new Vector3(0, 0, angleDeg-90);
+            //Debug.Log(billionColor + " spawned at: " + startPos + " | billion at: " + transform.position + " | actually at: " + bullet.transform.position);
+
+            // give bullet necessary information
+            //bullet.GetComponent<BillionBullet>().bulletColor = billionColor;
+            bullet.GetComponent<BillionBullet>().startPosition = startPos;
+            bullet.GetComponent<BillionBullet>().direction = diff.normalized;
+
+            // set time for next fire
+            nextFire = Time.time + shootInterval;
+        }
+        
+    }
+
+
+    private GameObject GetNearestEnemyBillion()
+    {
+        // get all billions on screen
+        GameObject[] allBillions = GameObject.FindGameObjectsWithTag("billion");
+
+        // make sure we return null if there are no enemy billions
+        if (allBillions.Length == 0 )
+        {
+            return null;
+        }
+
+        // get closest billion
+        GameObject nearestBillion = allBillions[0];
+        float nearestDist = Mathf.Infinity;
+        foreach (GameObject billion in allBillions)
+        {
+            float currentDist = Vector2.Distance(this.transform.position, billion.transform.position); // use vector2 distance so that z values aren't considered in calculation
+
+            // check that billion is enemy and that it is closer than the previous closest
+            if (billion.GetComponent<Billion>().billionColor != billionColor && currentDist < nearestDist)
+            {
+                nearestBillion = billion;
+                nearestDist = currentDist;
+            }
+        }
+
+        return nearestBillion;
     }
 }
---
+++ Lecture Project/Assets/Scripts/BillionBullet.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillionBullet : MonoBehaviour
+{
+    public string bulletColor; // the color of the bullet
+
+    public float bulletSpeed = 5f; // the speed at which a bullet will travel
+    public float bulletDistance = 10; // the max distance a bullet will travel
+    public float bulletDamage = 25; // the damage the bullet deals
+
+    public Vector3 startPosition; // the starting position of the bullet, set by billion when it is shot
+    public Vector2 direction; // the direction the bullet is heading
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // destroy gameobject if the bullet has traveled farther than the max distance
+        if (Vector2.Distance(startPosition, transform.position) >= bulletDistance)
+        {
+            Destroy(gameObject);
+        }
+
+        // move bulletSpeed units in direction every second
+        Vector3 deltaPos = direction.normalized * (bulletSpeed * Time.deltaTime);
+        deltaPos.z = 0;
+        transform.position += deltaPos;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if the object it hits is an enemy billion
+        if (other.gameObject.CompareTag("billion") && other.gameObject.GetComponent<Billion>().billionColor != bulletColor)
+        {
+            // deal damage to billion
+            other.gameObject.GetComponent<Billion>().TakeDamage(bulletDamage);
+
+            // destroy this gameObject
+            Destroy(gameObject);
+        }
+        // destroy the bullet if it hits a wall or a billionare base
+        else if (other.gameObject.CompareTag("wall") || other.gameObject.CompareTag("base"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
